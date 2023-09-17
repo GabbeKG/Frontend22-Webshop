ec30aba8817c369457a9251f7bdfaafd63aec44f
GabbeKG <el_sueco91@hotmail.com>
2023-09-17 18:32:21 +0200

    put, get and post added for orders and products.

--- Webshop - Backend/routers/orderRouter.js
+++ Webshop - Backend/routers/orderRouter.js
@@ -8,5 +8,24 @@ orderRouter
         const newOrder=await OrderModel.create(req.body);
         res.json(newOrder);
     });
+orderRouter
+    .get("/", async function(req,res){
+        const orders = await OrderModel.find();
+
+        res.json(orders);
+    })
+orderRouter
+    .put("/:id", async function(req,res){
+        const orderId={_id:req.body._id};
+        try {
+            const order= await OrderModel.findOneAndUpdate(orderId, {shipped:req.body.shipped}, {
+                new:true
+            });
+            
+            res.json(order)
+        } catch (error) {
+            console.log(error)
+        }
+    })
 
     module.exports = orderRouter;
--- Webshop - Backend/routers/productRouter.js
+++ Webshop - Backend/routers/productRouter.js
@@ -9,6 +9,26 @@ productRouter
         res.json(newProduct)
     });
 
+productRouter
+    .get("/", async function(req,res){
+        const products=await ProductModel.find();
+
+        res.json(products)
+    })
+productRouter
+    .put("/:id", async function (req,res){
+        const productId={_id:req.body._id};
+        console.log(productId)
+        try {
+            const product=await ProductModel.findOneAndUpdate(productId, {name:req.body.name, price:req.body.price},{
+                new:true
+            });
+            res.json(product)
+        } catch (error) {
+            console.log(error);
+        }
+    })
+
 
 
 module.exports =productRouter;
